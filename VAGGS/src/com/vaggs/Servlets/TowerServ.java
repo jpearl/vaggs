e048c66bfab68a5da92607f430c570fa96743cfb
Hawk (Laptop) <dazerdude@gmail.com>
2013-03-07 16:54:36 -0500

    Route Servlet & Updated Servlet Names

--- VAGGS/src/com/vaggs/Servlets/TowerServ.java
+++ VAGGS/src/com/vaggs/Servlets/TowerServ.java
@@ -1,10 +1,10 @@
-package com.vaggs;
+package com.vaggs.Servlets;
 
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class VAGGSServlet extends HttpServlet {
+public class TowerServ extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
