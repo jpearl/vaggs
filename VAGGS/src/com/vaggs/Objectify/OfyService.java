d943e56e44c36055e3706d69c18a75525191a6d6
Josh Pearl <josh@joshpearl.com>
2013-03-07 18:08:39 -0500

    add objectify service

---
+++ VAGGS/src/com/vaggs/Objectify/OfyService.java
@@ -0,0 +1,26 @@
+package com.vaggs.Objectify;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+import com.vaggs.AirportDiagram.Airport;
+import com.vaggs.Route.FavoriteRoutes;
+import com.vaggs.Route.Route;
+import com.vaggs.Route.RouteRequest;
+
+public class OfyService {
+    static {
+            factory().register(FavoriteRoutes.class);
+            factory().register(Airport.class);
+            factory().register(RouteRequest.class);
+            factory().register(Route.class);
+    }
+
+    public static Objectify ofy() {
+            return ObjectifyService.ofy();
+    }
+
+    public static ObjectifyFactory factory() {
+            return ObjectifyService.factory();
+    }
+}
